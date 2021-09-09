34952c5c77f065e3f7cb028a4d4542eb17b5b44a
AlokBharadwaj <alok@TUD255665.tudelft.net>
2021-09-09 21:48:09 +0200

    Mpi proper

--- utils/compute.py
+++ utils/compute.py
@@ -120,7 +120,7 @@ def get_central_scaled_pixel_vals_after_scaling(emmap, modmap, masked_xyz_locs, 
     
     if audit:
         profiles_audit = {}
-    for k, j, i in tqdm(masked_xyz_locs - wn / 2, desc="LocScale progress: "):
+    for k, j, i in tqdm(masked_xyz_locs - wn / 2, desc=process_name):
         k,j,i,wn = int(round(k)),int(round(j)),int(round(i)),int(round(wn))
         emmap_wn = emmap[k: k+wn, j: j+wn, i: i+ wn]
         modmap_wn = modmap[k: k+wn, j: j+wn, i: i+ wn]
