7d47f474317cae09c48147a49d67503973f3b595
AlokBharadwaj <alok@TUD255665.tudelft.net>
2021-09-09 23:18:15 +0200

    comm barrier each iteration

--- utils/compute.py
+++ utils/compute.py
@@ -173,11 +173,12 @@ def get_central_scaled_pixel_vals_after_scaling(emmap, modmap, masked_xyz_locs, 
         
         if mpi:
             pbar[rank].update(1)
+            pbar = {}
         else:
             progress_bar.update(n=1)
         
     if mpi:
-        comm.barrier()
+    #    comm.barrier()
     if audit:
         with open("profiles_audit.pickle","wb") as audit:
             pickle.dump(profiles_audit, audit)
