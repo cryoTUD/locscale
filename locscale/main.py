dd68f9272816c4b86265e10359e7a35f30832afd
AlokBharadwaj <alok@TUD255665.tudelft.net>
2021-09-17 01:05:38 +0200

    Changed some print statements

--- locscale/main.py
+++ locscale/main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from argparse import RawTextHelpFormatter
+from datetime import datetime
 #from mpi4py import MPI
 
 progname = os.path.basename(sys.argv[0])
@@ -9,37 +10,46 @@ datmod = "2021-09-07"  # to be updated by gitlab after every commit
 author = '\n\nAuthors: Arjen J. Jakobi, Carsten Sachse, EMBL and Alok Bharadwaj'
 version = progname + '  0.2' + '  (;' + datmod+ ')'
 
-simple_cmd = 'python np_locscale.py -em emmap.mrc -mm modmap.mrc -ma mask.mrc -w 25 -o loc_scaled.mrc'
+simple_cmd = 'python main.py -em emmap.mrc -mm modmap.mrc -ma mask.mrc -w 25 -o loc_scaled.mrc'
+
+description = "*** Computes contrast-enhanced cryo-EM maps by local amplitude scaling using a reference map *** \n \
+Update: Now perform local amplitude scaling without an explicit reference to an atomic model"
 
 cmdl_parser = argparse.ArgumentParser(
-description='*** Computes contrast-enhanced cryo-EM maps by local amplitude scaling using a reference model ***\n' + \
+description='*** Computes contrast-enhanced cryo-EM maps by local amplitude scaling using a reference map.  ***\n \
+Update: Now perform local amplitude scaling without an explicit reference to an atomic model' + \
 ('\nExample usage: \"{0}\". {1} on {2}'.format(simple_cmd, author, datmod)),formatter_class=RawTextHelpFormatter)
 
 
-mpi_cmd = 'mpirun -np 4 python locscale_mpi.py -em emmap.mrc -mm modmap.mrc -ma mask.mrc -p 1.0 -w 10 -mpi -o scaled.mrc'
+mpi_cmd = 'mpirun -np 4 python main.py -em emmap.mrc -mm modmap.mrc -ma mask.mrc -p 1.0 -w 10 -mpi -o scaled.mrc'
+
+simple_cmd_use_pm = 'python main.py -em emmap.mrc -ma mask.mrc -o loc_scaled.mrc -use_pm'
+
+
 
 cmdl_parser.add_argument('-em', '--em_map', required=True, help='Input filename EM map')
 cmdl_parser.add_argument('-mm', '--model_map', help='Input filename PDB map')
-cmdl_parser.add_argument('-p', '--apix', type=float, help='pixel size in Angstrom')
 cmdl_parser.add_argument('-ma', '--mask', help='Input filename mask')
 cmdl_parser.add_argument('-mc', '--model_coordinates', help='Input PDB files', default=None)
-cmdl_parser.add_argument('-w', '--window_size', type=int, help='window size in pixels', default=None)
-cmdl_parser.add_argument('-res', '--ref_resolution', required=True, type=float, help='Resolution target for Refmac refinement')
-cmdl_parser.add_argument('-sym', '--symmetry', default='C1', type=str, help='Impose symmetry condition for output')
 cmdl_parser.add_argument('-o', '--outfile', required=True, help='Output filename')
+cmdl_parser.add_argument('-res', '--ref_resolution', required=True, type=float, help='Resolution target for Refmac refinement')
+cmdl_parser.add_argument('-p', '--apix', type=float, help='pixel size in Angstrom')
+cmdl_parser.add_argument('-wn', '--window_size', type=int, help='window size in pixels', default=None)
+cmdl_parser.add_argument('-fdr_w', '--fdr_window_size', type=int, help='window size in pixels for FDR thresholding', default=None)
+cmdl_parser.add_argument('-fdr_f', '--fdr_filter', type=float, help='Pre-filter for FDR thresholding', default=None)
+cmdl_parser.add_argument('-use_pm', '--use_pseudomaps', help='Use pseudo-atomic model', action='store_true')
+cmdl_parser.add_argument('-dst', '--distance', type=float, help='For pseudo-atomic model: typical distance between atoms', default=1.2)
+cmdl_parser.add_argument('-pm', '--pseudomodel_method', help='For pseudo-atomic model: method', default='gradient')
+cmdl_parser.add_argument('-pm_it', '--total_iterations', type=int, help='For pseudo-atomic model: total iterations', default=None)
+cmdl_parser.add_argument('-ref_it', '--refmac_iterations', help='For pseudo-atomic model: number of refmac iterations', default=15)
+cmdl_parser.add_argument('-sym', '--symmetry', default='C1', type=str, help='Impose symmetry condition for output')
 cmdl_parser.add_argument('-mpi', '--mpi', action='store_true', default=False,
                          help='MPI version call by: \"{0}\"'.format(mpi_cmd))
-cmdl_parser.add_argument('-fdr_w', '--fdr_window_size', type=int, help='window size in pixels for FDR thresholding', default=None)
-cmdl_parser.add_argument('-dst', '--distance', type=float, help='For pseudo-atomic model: typical distance between atoms', default=1.2)
-cmdl_parser.add_argument('-fdr_f', '--fdr_filter', type=float, help='Pre-filter for FDR thresholding', default=None)
-cmdl_parser.add_argument('-pm', '--pseudomodel_method', help='For pseudo-atomic model: method', default='gradient')
-cmdl_parser.add_argument('-ref_it', '--refmac_iterations', help='For pseudo-atomic model: number of refmac iterations', default=15)
-cmdl_parser.add_argument('-pm_it', '--total_iterations', type=int, help='For pseudo-atomic model: total iterations', default=None)
 cmdl_parser.add_argument('-b_global', '--global_bfactor', type=int, help='Globally sharpen the map', default=0)
 cmdl_parser.add_argument('-s', '--smooth_factor', type=float, help='Smooth factor for merging profiles', default=0.3)
 cmdl_parser.add_argument('-v', '--verbose', default=True,
                          help='Verbose output')
-cmdl_parser.add_argument('-use_pm', '--use_pseudomaps', help='Use pseudo-atomic model', action='store_true')
+
 
 def print_arguments(args):
     print('\n  LocScale Arguments\n')
@@ -53,52 +63,91 @@ def launch_amplitude_scaling(args):
     from locscale.utils.scaling_tools import run_window_function_including_scaling, run_window_function_including_scaling_mpi, write_out_final_volume_window_back_if_required
     
     if not args.mpi:
+        start_time = datetime.now()
+        print("Launching amplitude scaling\n")
+        print(start_time)
+        print("**************************************************")
+        print("*                                                *")
+        print("*                      LOCSCALE                  *")
+        print("*                                                *")
+        print("**************************************************")
         if args.verbose:
             print_arguments(args)
-        parsed_arguments = prepare_mask_and_maps_for_scaling(args)
+            
+        parsed_inputs_dict = prepare_mask_and_maps_for_scaling(args)
         
-        input_to_scaling = parsed_arguments[:-1]
+        LocScaleVol = run_window_function_including_scaling(parsed_inputs_dict)
         
-        LocScaleVol = run_window_function_including_scaling(*input_to_scaling)
+        write_out_final_volume_window_back_if_required(args, LocScaleVol, parsed_inputs_dict)
         
-        window_bleed_and_pad = parsed_arguments[-1]
-        wn = parsed_arguments[3]
-        apix = parsed_arguments[4]
-        write_out_final_volume_window_back_if_required(args, wn, window_bleed_and_pad, LocScaleVol, apix=apix)
+        '''
+        ** INFORMATION ABOUT parsed_inputs_dict VARIABLE **
+        parsed_inputs_dict.keys()
+        ['emmap'] : numpy.ndarray   | Experimental Map
+        ['modmap'] : numpy.ndarray  | Reference Map for scaling
+        ['mask'] : numpy.ndarray    | Mask 
+        ['wn'] :  int               | Window size, LocScale (pix) 
+        ['apix'] : float            | Pixel size 
+        ['use_theoretical'] : bool  | Flag to use theoretical       profiles
+        ['scale_factor_args'] : dict| Parameters for scale factor computation
+        
+        ['verbose'] : bool          | Verbose parameter
+        ['win_bleed_pad'] : bool    | Window bleed and pad         
+        '''
         
         print("You can find the scaled map here: {}".format(args.outfile))
-        
+        print("Finished amplitude scaling\n")
+        print("Processing time: {}".format(datetime.now()-start_time))
+        print("**************************************************")
+        print("*                                                *")
+        print("*         Good luck with your research!          *")
+        print("*                                                *")
+        print("**************************************************")
+            
     
     elif args.mpi:
         from mpi4py import MPI
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
-        size = comm.Get_size()
+        
         
         if rank==0:
+            start_time = datetime.now()
+            print("Launching amplitude scaling\n")
+            print(start_time)
+            print("**************************************************")
+            print("*                                                *")
+            print("*                      LOCSCALE                  *")
+            print("*                                                *")
+            print("**************************************************")
             if args.verbose:
                 print_arguments(args)
             
-            parsed_arguments = prepare_mask_and_maps_for_scaling(args)
+            parsed_inputs_dict = prepare_mask_and_maps_for_scaling(args)
         
         else:
-            parsed_arguments = None
+            parsed_inputs_dict = None
         
         comm.barrier()
         
-        parsed_arguments = comm.bcast(parsed_arguments, root=0)
+        parsed_inputs_dict = comm.bcast(parsed_inputs_dict, root=0)
     
-        input_to_scaling = parsed_arguments[:-1]
+        #input_to_scaling = parsed_arguments[:-1]
         
-        LocScaleVol, rank = run_window_function_including_scaling_mpi(*input_to_scaling)
+        LocScaleVol, rank = run_window_function_including_scaling_mpi(parsed_inputs_dict)
         
         if rank == 0:
-            window_bleed_and_pad = parsed_arguments[-1]
-            wn = parsed_arguments[3]
-            apix = parsed_arguments[4]
-            write_out_final_volume_window_back_if_required(args, wn, window_bleed_and_pad, LocScaleVol, apix=apix)
+            write_out_final_volume_window_back_if_required(args, LocScaleVol, parsed_inputs_dict)
     
             print("You can find the scaled map here: {}".format(args.outfile))
+            print("Finished amplitude scaling\n")
+            print("Processing time: {}".format(datetime.now()-start_time))
+        print("**************************************************")
+        print("*                                                *")
+        print("*         Good luck with your research!          *")
+        print("*            from CPU: {}                        *".format(rank))
+        print("**************************************************")
+                
 
 
 def main():
