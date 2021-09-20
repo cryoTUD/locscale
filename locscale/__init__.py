5e0284222e00f5c4588e3a6af255b11107e2d08b
Arjen Jakobi <a.jakobi@tudelft.nl>
2021-09-20 21:24:26 +0000

    Add version to __init__.py

--- locscale/__init__.py
+++ locscale/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+__version__ = '0.2'
 import locscale.include.emmer as emmer
 #import locscale.include.emda as emda
 import locscale.include.confidenceMapUtil as confidenceMapUtil
