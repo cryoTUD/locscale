46e9acb885ec7d15899d2fc8909125467c6d08e5
abharadwaj1 <alok@TUD212103.tudelft.net>
2022-03-02 13:00:47 +0100

    Outputs a report if flag is given

--- locscale/scripts/run_locscale_multiple_pdb.py
+++ locscale/scripts/run_locscale_multiple_pdb.py
@@ -19,8 +19,9 @@ output_folder = "/home/abharadwaj1/dev/faraday_tests/output_folder"
 
 halfmap_1_path = os.path.join(input_folder, "emd_10692_half_map_1.map")
 halfmap_2_path = os.path.join(input_folder, "emd_10692_half_map_2.map")
+mask_path = os.path.join(input_folder, "emdb_map_unfiltered_confidenceMap.mrc")
 
-perturb_magnitudes = [0, 1, 2, 5, 7, 10, 12, 15, 17, 20]
+perturb_magnitudes = [1, 2, 5, 7, 10, 12, 15, 17, 20]
 
 pdb_paths = {}
 locscale_paths = {}
@@ -28,7 +29,7 @@ processing_files_folder = {}
 
 for perturb in perturb_magnitudes:    
     pdb_paths[perturb] = os.path.join(input_folder,"pdb6y5a_perturbed_{}_A.pdb".format(perturb))
-    locscale_paths[perturb] = os.path.join(output_folder,"locscale_perturbed_pdb_{}A".format(perturb))
+    locscale_paths[perturb] = os.path.join(output_folder,"locscale_perturbed_pdb_{}A.mrc".format(perturb))
     processing_files_folder[perturb] = os.path.join(output_folder, "processing_files_{}A".format(perturb))
     
 
@@ -46,7 +47,7 @@ print("Running the following scripts: \n")
 locscale_run_scripts = {}
 for perturb in perturb_magnitudes:
     print("\n################\n")
-    script = ["mpirun","-np","4","python",locscale_script,"--half_map1",halfmap_1_path, "--half_map2",halfmap_2_path,
+    script = ["mpirun","-np","4","python",locscale_script,"--half_map1",halfmap_1_path, "--half_map2",halfmap_2_path,"--mask_path",mask_path,
               "--ref_resolution",resolution, "--model_resolution",model_resolution,"--verbose","--mpi",
               "--model_coordinates",pdb_paths[perturb],"--outfile",locscale_paths[perturb],
               "--output_processing_files",processing_files_folder[perturb]]
@@ -58,7 +59,7 @@ print("\n############### START ###################\n")
 
 for perturb in perturb_magnitudes:
     return_command = run(locscale_run_scripts[perturb], check=True)
-    
+
     print("===========Locscale run finished for perturb {} A ====== ".format(perturb))
     print("Exit code: ",return_command.returncode)
     
