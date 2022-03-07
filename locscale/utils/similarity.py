ae6436f8c1aa1067cab08dc9f01a78f80be0a6f8
abharadwaj1 <alok@TUD212103.tudelft.net>
2022-03-07 19:36:28 +0100

    More update on local resolution analysis

--- locscale/utils/similarity.py
+++ locscale/utils/similarity.py
@@ -9,23 +9,30 @@ import numpy as np
 import mrcfile
 
 
-def similarity_threshold(reference_map, threshold_reference, target_map, num_bins=100):
+def similarity_threshold(reference_map, threshold_reference, target_map, num_bins=100, min_threshold_target=None):
     from emmer.ndimage.map_tools import compute_real_space_correlation as rsc
+    from tqdm import tqdm
+    
     
     
     def binarizeMap(emmap, reference):
         return (emmap>reference).astype(np.int_)
-    threshold_range_target = np.linspace(0, target_map.max(), num_bins)
+    
+    if min_threshold_target is None:
+        min_threshold_target = 0
+    else:
+        min_threshold_target = min_threshold_target
+    threshold_range_target = np.linspace(min_threshold_target, target_map.max(), num_bins)
     
     binarised_reference = binarizeMap(reference_map,threshold_reference)
     rscc = {}
-    for threshold in threshold_range_target:
+    for threshold in tqdm(threshold_range_target, desc="Thresholding..."):
         binarised_target = binarizeMap(target_map, threshold)
         rscc[threshold] = rsc(binarised_reference, binarised_target)
     
     return rscc
 
-def find_threshold(reference_map_path, reference_threshold, list_of_target_map_paths, mask_path=None, num_bins=100):
+def find_threshold(reference_map_path, reference_threshold, list_of_target_map_paths, mask_path=None, num_bins=100,min_threshold_target=None):
     import matplotlib.pyplot as plt
     
     reference_map = mrcfile.open(reference_map_path).data
@@ -41,7 +48,7 @@ def find_threshold(reference_map_path, reference_threshold, list_of_target_map_p
         if mask_path is not None:
             target_map = target_map * mask
         
-        rsc_curve = similarity_threshold(reference_map, threshold_reference=reference_threshold, target_map=target_map)
+        rsc_curve = similarity_threshold(reference_map, threshold_reference=reference_threshold, target_map=target_map, min_threshold_target=min_threshold_target)
         rscc_curve[map_name] = rsc_curve
         target_threshold[map_name] = max(rsc_curve, key=rsc_curve.get)
         plt.plot(rsc_curve.keys(), rsc_curve.values())
