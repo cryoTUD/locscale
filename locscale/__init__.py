a892d19fa3f1cab17887f5f25fa8a35a8b6c5cd4
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-11 14:37:10 +0200

    edited init script

--- locscale/__init__.py
+++ locscale/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __version__ = '0.2'
-import locscale.include.emmer as emmer
+#import locscale.include.emmer as emmer
 #import locscale.include.emda as emda
-import locscale.include.confidenceMapUtil as confidenceMapUtil
+#import locscale.include.confidenceMapUtil as confidenceMapUtil
 
