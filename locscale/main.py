9607808c72f4fb518995c525e4e92c82d47e714b
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-18 21:34:38 +0200

    CLI args set for locscale and emmernet integration

--- locscale/main.py
+++ locscale/main.py
@@ -3,86 +3,201 @@ import sys
 import argparse
 from argparse import RawTextHelpFormatter
 from datetime import datetime
-#from mpi4py import MPI
+import pyfiglet
 
 progname = os.path.basename(sys.argv[0])
-datmod = "2021-09-07"  # to be updated by gitlab after every commit
-author = '\n\nAuthors: Arjen J. Jakobi, Carsten Sachse, EMBL and Alok Bharadwaj'
-version = progname + '  0.2' + '  (;' + datmod+ ')'
+author = '\n\nAuthors: Arjen J. Jakobi (TU Delft), Carsten Sachse (EMBL),  Alok Bharadwaj (TU Delft) and Reinier de Bruin (TU Delft)\n\n'
+version = progname + '  0.2'
 
-simple_cmd = 'python main.py -em emmap.mrc -mm modmap.mrc -ma mask.mrc -w 25 -o loc_scaled.mrc'
-
-description = "*** Computes contrast-enhanced cryo-EM maps by local amplitude scaling using a reference map *** \n \
-Update: Now perform local amplitude scaling without an explicit reference to an atomic model"
-
-cmdl_parser = argparse.ArgumentParser(
-description='*** Computes contrast-enhanced cryo-EM maps by local amplitude scaling using a reference map.  ***\n \
-Update: Now perform local amplitude scaling without an explicit reference to an atomic model' + \
-('\nExample usage: \"{0}\". {1} on {2}'.format(simple_cmd, author, datmod)),formatter_class=RawTextHelpFormatter)
-
-
-mpi_cmd = 'mpirun -np 4 python main.py -em emmap.mrc -mm modmap.mrc -ma mask.mrc -p 1.0 -w 10 -mpi -o scaled.mrc'
-
-simple_cmd_use_pm = 'python main.py -em emmap.mrc -ma mask.mrc -o loc_scaled.mrc -use_pm'
+description = "*** A comprehensive software to optimise amplitude contrast in cryo-EM density map using both sharpening and machine learning prediction *** \n "
 
 
 
-cmdl_parser.add_argument('-em', '--em_map',  help='Input filename EM map')
-cmdl_parser.add_argument('-hf1', '--half_map1', help='Input filename first half map')
-cmdl_parser.add_argument('-hf2', '--half_map2', help='Input filename second half map')
-cmdl_parser.add_argument('-mm', '--model_map', help='Input filename PDB map')
-cmdl_parser.add_argument('-ma', '--mask', help='Input filename mask')
-cmdl_parser.add_argument('-mc', '--model_coordinates', help='Input PDB files', default=None)
-cmdl_parser.add_argument('-mw', '--molecular_weight', help='Input molecular weight (in kDa)', default=None)
-cmdl_parser.add_argument('-o', '--outfile', help='Output filename')
-cmdl_parser.add_argument('-op', '--output_processing_files', type=str, help='Path to store processing files', default="processing_files")
-cmdl_parser.add_argument('-res', '--ref_resolution', type=float, help='Resolution target for Refmac refinement')
-cmdl_parser.add_argument('-mres', '--model_resolution', type=float, help='Resolution limit for Model Map generation')
-cmdl_parser.add_argument('-p', '--apix', type=float, help='pixel size in Angstrom')
-cmdl_parser.add_argument('-wn', '--window_size', type=int, help='window size in pixels', default=None)
-cmdl_parser.add_argument('-fdr_w', '--fdr_window_size', type=int, help='window size in pixels for FDR thresholding', default=None)
-cmdl_parser.add_argument('-fdr_f', '--fdr_filter', type=float, help='Pre-filter for FDR thresholding', default=None)
-cmdl_parser.add_argument('--ignore_profiles', help='Ignore average secondary structure profile during local scaling', action='store_true')
-cmdl_parser.add_argument('--skip_refine', help='Ignore REFMAC refinement', action='store_true')
-cmdl_parser.add_argument('-dst', '--distance', type=float, help='For pseudo-atomic model: typical distance between atoms', default=1.2)
-cmdl_parser.add_argument('-pm', '--pseudomodel_method', help='For pseudo-atomic model: method', default='gradient')
-cmdl_parser.add_argument('--build_ca_only', help='For gradient pseudomodel building: use only Ca atoms with interatomic distance 3.8', action='store_true',default=False)
-cmdl_parser.add_argument('-pm_it', '--total_iterations', type=int, help='For pseudo-atomic model: total iterations', default=None)
-cmdl_parser.add_argument('-ref_it', '--refmac_iterations', help='For pseudo-atomic model: number of refmac iterations', default=15)
-cmdl_parser.add_argument('-sym', '--symmetry', default='C1', type=str, help='Impose symmetry condition for output')
-cmdl_parser.add_argument('-mpi', '--mpi', action='store_true', default=False,
-                         help='MPI version call by: \"{0}\"'.format(mpi_cmd))
-cmdl_parser.add_argument('--add_blur', type=int, help='Globally sharpen the map', default=0)
-cmdl_parser.add_argument('-s', '--smooth_factor', type=float, help='Smooth factor for merging profiles', default=0.3)
-cmdl_parser.add_argument('--boost_secondary_structure', type=float, help='Amplify signal corresponding to secondary structures', default=1.5)
-cmdl_parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                         help='Verbose output')
-cmdl_parser.add_argument('--dev_mode', action='store_true', default=False,
-                         help='If true, this will force locscale to use the theoretical profile even if model map present and will not check for user input consistency')
-cmdl_parser.add_argument('--output_report', action='store_true', default=False,
-                         help='Print a PDF copy of the report')
-cmdl_parser.add_argument('--report_filename', type=str, help='Filename for storing PDF output and statistics', default="locscale_report")
-cmdl_parser.add_argument('--no_reference', action='store_true', default=False,
-                         help='Run locscale without using any reference information')
+main_parser = argparse.ArgumentParser(
+description=description )
+
+## Add verbose argument to main parser
+main_parser.add_argument('-v', '--verbose', action='store_true',help='Verbose output')
+main_parser.add_argument('-o', '--outfile', help='Output filename')
+
+sub_parser = main_parser.add_subparsers(dest='command')
+locscale_parser = sub_parser.add_parser('run_locscale', help='Run LocScale')
+emmernet_parser = sub_parser.add_parser('run_emmernet', help='Run EMMERNET')
+
+# **************************************************************************************
+# ************************ Command line arguments LocScale *****************************
+# **************************************************************************************
+
+## Input either unsharpened EM map or two halfmaps
+locscale_emmap_input = locscale_parser.add_mutually_exclusive_group(required=True)
+locscale_emmap_input.add_argument('-em', '--emmap_path',  help='Path to unsharpened EM map')
+locscale_emmap_input.add_argument('-hm', '--halfmap_paths', help='Paths to first and second halfmaps')
+
+## Input model map file (mrc file) or atomic model (pdb file)
+locscale_parser.add_argument('-mm', '--model_map', help='Path to model map file')
+locscale_parser.add_argument('-mc', '--model_coordinates', help='Path to PDB file', default=None)
+
+## Input mask
+locscale_parser.add_argument('-ma', '--mask', help='Input filename mask')
+
+## Output arguments
+locscale_parser.add_argument('-o', '--outfile', help='Output filename')
+locscale_parser.add_argument('-v', '--verbose', action='store_true',help='Verbose output')
+locscale_parser.add_argument('--output_report', action='store_true', default=False,help='Print a PDF copy of the report')
+locscale_parser.add_argument('--report_filename', type=str, help='Filename for storing PDF output and statistics', default="locscale_report")
+locscale_parser.add_argument('-op', '--output_processing_files', type=str, help='Path to store processing files', default="processing_files")
+
+## LocScale main function parameters
+locscale_parser.add_argument('-wn', '--window_size', type=int, help='window size in pixels', default=None)
+locscale_parser.add_argument('-mpi', '--mpi', action='store_true', default=False,help='MPI version')
+locscale_parser.add_argument('-np', '--number_processes', help='Number of processes to use', type=int, default=1)
+
+## Refinement parameters
+locscale_parser.add_argument('-ref_it', '--refmac_iterations', help='For atomic model refinement: number of refmac iterations', default=15)
+locscale_parser.add_argument('-res', '--ref_resolution', type=float, help='Resolution target for Refmac refinement')
+locscale_parser.add_argument('-p', '--apix', type=float, help='pixel size in Angstrom')
+locscale_parser.add_argument('--add_blur', type=int, help='Globally sharpen the target map for REFMAC refinement', default=0)
+
+## Model map parameters
+locscale_parser.add_argument('-mres', '--model_resolution', type=float, help='Resolution limit for Model Map generation')
+locscale_parser.add_argument('-sym', '--symmetry', default='C1', type=str, help='Impose symmetry condition for output')
+
+## FDR parameters
+locscale_parser.add_argument('-fdr_w', '--fdr_window_size', type=int, help='window size in pixels for FDR thresholding', default=None)
+locscale_parser.add_argument('-fdr_f', '--fdr_filter', type=float, help='Pre-filter for FDR thresholding', default=None)
+
+## Pseudo-atomic model method parameters
+locscale_parser.add_argument('-pm', '--pseudomodel_method', help='For pseudo-atomic model: method', default='gradient')
+locscale_parser.add_argument('-pm_it', '--total_iterations', type=int, help='For pseudo-atomic model: total iterations', default=None)
+locscale_parser.add_argument('-dst', '--distance', type=float, help='For pseudo-atomic model: typical distance between atoms', default=1.2)
+locscale_parser.add_argument('-mw', '--molecular_weight', help='Input molecular weight (in kDa)', default=None)
+locscale_parser.add_argument('--build_ca_only', help='For gradient pseudomodel building: use only Ca atoms with interatomic distance 3.8', action='store_true',default=False)
+locscale_parser.add_argument('-s', '--smooth_factor', type=float, help='Smooth factor for merging profiles', default=0.3)
+locscale_parser.add_argument('--boost_secondary_structure', type=float, help='Amplify signal corresponding to secondary structures', default=1.5)
+locscale_parser.add_argument('--no_reference', action='store_true', default=False,help='Run locscale without using any reference information')
+
+## non-default arguments
+locscale_parser.add_argument('--dev_mode', action='store_true', default=False,help='If true, this will force locscale to use the theoretical profile even if model map present and will not check for user input consistency')
+locscale_parser.add_argument('--ignore_profiles', help='Ignore average secondary structure profile during local scaling', action='store_true')
+locscale_parser.add_argument('--skip_refine', help='Ignore REFMAC refinement', action='store_true')
+
+# **************************************************************************************
+# ************************ Command line arguments EMMERNET *******************************
+# **************************************************************************************
+
+## Input either unsharpened EM map or two halfmaps
+emmernet_emmap_input = emmernet_parser.add_mutually_exclusive_group(required=True)
+emmernet_emmap_input.add_argument('-em', '--emmap_path',  help='Path to unsharpened EM map')
+emmernet_emmap_input.add_argument('-hm', '--halfmap_paths', help='Paths to first and second halfmaps')
+
+## Output arguments
+emmernet_parser.add_argument('-o', '--outfile', help='Output filename')
+emmernet_parser.add_argument('-op', '--output_processing_files', type=str, help='Path to store processing files', default="processing_files")
+emmernet_parser.add_argument('-v', '--verbose', action='store_true',help='Verbose output')
+
+## Emmernet main function parameters
+emmernet_parser.add_argument('-trained_model','--trained_model', help='Type of emmernet model to use', \
+                            choices=['model_based', 'model_free', 'ensemble'], default='model_based')
+emmernet_parser.add_argument('-s', '--stride', help='Stride for EMMERNET', default=16, type=int)
+emmernet_parser.add_argument('-bs', '--batch_size', type=int, help='Batch size for EMMERNET', default=8)
+emmernet_parser.add_argument("-gpus", "--gpu_ids", nargs='+', help="numbers of the selected GPUs, format: '1 2 3 ... 5'", required=True)
+
+
 
 def print_arguments(args):
-    print('\n  LocScale Arguments\n')
+    print("."*80)
+    print('Input Arguments')
+    print("."*80)
     for arg in vars(args):
-        print('    {} : {}'.format(arg, getattr(args, arg)))        
-        
-def print_start_banner(start_time):
-    print("Launching amplitude scaling\n")
-    print(start_time)
-    print("**************************************************")
-    print("*                      LOCSCALE                  *")
-    print("**************************************************")
+        print('\t{}:  {}'.format(arg, getattr(args, arg)))        
+    print("."*80)
+
+def print_start_banner(start_time, text="Map Sharpening"):
+    from textwrap import fill
+    ## Definitions
+    try:
+        username = os.environ.get("USER")
+    except:
+        username = "Unknown"
+
+    ## get today's date from start_time
+    today_date = start_time.strftime("%d-%m-%Y")
+    time_now = start_time.strftime("%H:%M:%S")
+
+    ## Author credits
+    author_list = ["Arjen J. Jakobi (TU Delft)", "Carsten Sachse (EMBL)",  "Alok Bharadwaj (TU Delft)", "Reinier de Bruin (TU Delft)"]
+
+    if text == "LocScale":
+        version = "2.0"
+    elif text == "EMmerNet":
+        version = "1.0"
+    else:
+        version = "x"
+
+    ## Paper reference
+    paper_ref_1 = "Arjen J Jakobi, Matthias Wilmanns, Carsten Sachse (2017), \"Model-based local density sharpening of cryo-EM maps\"" 
+    paper_ref_2 = "Alok Bharadwaj, Arjen J Jakobi (2022), \"Electron scattering properties of biological macromolecules and their use for cryo-EM map sharpening\""
+    paper_ref_3 = "Alok Bharadwaj, Reinier de Bruin, Arjen J Jakobi (2022), \"TBD\""
+    print("="*80)
+    print("="*80)
+    result = pyfiglet.figlet_format(text, font = "slant")
+    print(result)
+    print("\t"*6 + "Version: v{}".format(version))
+    print("."*80)
+    # Print user info and current time
+    print("  |  ".join(["User: {}".format(username), "Date: {}".format(today_date), "Time: {}".format(time_now)]))
+    print("\n")
+    # Print author credits
+    print("Credits:\n")
+    for author in author_list:
+        print("\t{} \n".format(author))
+    # Print paper references
+    print("References:\n")
+    print(fill("{}".format(paper_ref_1), width=80, subsequent_indent="\t"))
+    print(fill("{}".format(paper_ref_2), width=80, subsequent_indent="\t"))
+    #print(wrap("{}".format(paper_ref_3), width=80))
+    print("\n")
+    print("="*80)
+    print("="*80)
+    
 
 def print_end_banner(time_now, start_time):
-    print("Finished amplitude scaling\n")
-    print("Processing time: {}".format(time_now-start_time))
-    print("**************************************************")
-    print("*         Good luck with your research!          *")
-    print("**************************************************")
+    print("."*80)
+    ## print processing time in minutes
+    print("Processing time: {:.2f} minutes".format((time_now-start_time).total_seconds()/60))
+    print("="*80)
+    print("Dank je wel!")
+    print("="*80)
+    
+
+def launch_emmernet(args):
+    from locscale.emmernet.utils import check_emmernet_inputs, check_and_save_output
+    from locscale.utils.general import change_directory
+    from locscale.emmernet.prepare_inputs import prepare_inputs
+    from locscale.emmernet.run_emmernet import run_emmernet
+    
+    ## Print start
+    start_time = datetime.now()
+    print_start_banner(start_time, "EMmerNet")
+    if args.verbose:
+        print_arguments(args)
+    
+    ## Check input
+    check_emmernet_inputs(args)
+
+    ## Change to output directory
+    current_directory = os.getcwd()
+    copied_args = change_directory(args, args.output_processing_files)  ## Copy the contents of files into a new directory
+    ## Prepare inputs
+    input_dictionary = prepare_inputs(copied_args)
+    ## Run EMMERNET
+    emmernet_output_dictionary = run_emmernet(input_dictionary)
+    ## Check and save output
+    os.chdir(current_directory)
+    check_and_save_output(input_dictionary, emmernet_output_dictionary)
+
+    ## Print end
+    print_end_banner(datetime.now(), start_time)
 
 
 def launch_amplitude_scaling(args):
@@ -102,7 +217,7 @@ def launch_amplitude_scaling(args):
     if not args.mpi:
         check_user_input(args)   ## Check user inputs
         start_time = datetime.now()
-        print_start_banner(start_time)
+        print_start_banner(start_time, "LocScale")
         if args.verbose:
             print_arguments(args)
         copied_args = change_directory(args, args.output_processing_files)  ## Copy the contents of files into a new directory
@@ -113,35 +228,9 @@ def launch_amplitude_scaling(args):
         os.chdir(current_directory)
         write_out_final_volume_window_back_if_required(copied_args, LocScaleVol, parsed_inputs_dict)
         
-        '''
-        ** INFORMATION ABOUT parsed_inputs_dict VARIABLE **
-        parsed_inputs_dict.keys()
-        ['emmap'] : numpy.ndarray   | Experimental Map
-        ['modmap'] : numpy.ndarray  | Reference Map for scaling
-        ['mask'] : numpy.ndarray    | Mask 
-        ['wn'] :  int               | Window size, LocScale (pix) 
-        ['apix'] : float            | Pixel size 
-        ['use_theoretical'] : bool  | Flag to use theoretical       profiles
-        ['scale_factor_args'] : dict| Parameters for scale factor computation
-        
-        ['verbose'] : bool          | Verbose parameter
-        ['win_bleed_pad'] : bool    | Window bleed and pad         
-        
-        ['bfactor_info'] : list     | Information about bfactors, slopes and breakpoints from PWLF
-        ['fsc_resolution'] : float  | FSC resolution
-        ['PWLF_fit'] : float        | R^2 of PWLF for the radial profile of the input EM map
-        ['emmap_path'] : str        | Em map path (after running mapmask command)
-        ['mask_path'] : str         | Mask path (after running mapmask command)
-        
-        '''
-        
         print("You can find the scaled map here: {}".format(args.outfile))
         print_end_banner(datetime.now(), start_time=start_time)
-        
-        
 
-            
-    
     elif args.mpi:
         from mpi4py import MPI
         comm = MPI.COMM_WORLD
@@ -177,13 +266,20 @@ def launch_amplitude_scaling(args):
                 print_end_banner(datetime.now(), start_time=start_time)
         except Exception as e:
             print(e)
-            
+
         
-
+        
 def main():
-    args = cmdl_parser.parse_args()
+    main_args = main_parser.parse_args()
+    
+    launch_command = main_args.command
 
-    launch_amplitude_scaling(args)
+    if launch_command == 'run_emmernet':
+        launch_emmernet(main_args)
+    elif launch_command == 'run_locscale':
+        
+        launch_amplitude_scaling(main_args)
+    
 
 if __name__ == '__main__':
     main()
