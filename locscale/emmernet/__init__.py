d20703b30c64e1f50a81d236f6e9517eecfa72be
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-18 21:36:30 +0200

    Emmernet module added

---
+++ locscale/emmernet/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__version__ = '0.2'
+#import locscale.include.emmer as emmer
+#import locscale.include.emda as emda
+#import locscale.include.confidenceMapUtil as confidenceMapUtil
+
---
+++ locscale/emmernet/prepare_inputs.py
@@ -0,0 +1,44 @@
+import mrcfile
+import os
+
+def prepare_inputs(args):
+    from locscale.emmernet.utils import check_emmernet_dependencies
+    from locscale.utils.general import get_emmap_path_from_args
+    from locscale.pseudomodel.pseudomodel_headers import run_mapmask
+    print("."*80)
+    check_emmernet_dependencies(verbose=True)
+
+    emmap_path, _ = get_emmap_path_from_args(args)
+
+    xyz_emmap_path = run_mapmask(emmap_path)
+    emmernet_type = args.trained_model
+    stride = args.stride
+    verbose = args.verbose
+    outputfile = args.outfile
+    batch_size = args.batch_size
+    gpu_ids = args.gpu_ids
+
+
+
+    inputs_dictionary = {
+        "emmap_path": emmap_path,
+        'xyz_emmap_path': xyz_emmap_path,
+        "emmernet_type": emmernet_type,
+        "stride": stride,
+        "verbose": verbose,
+        "outfile": outputfile,
+        "batch_size": batch_size,
+        "gpu_ids": gpu_ids,
+        }
+    
+    if verbose:
+        print("Inputs parsed successfully")
+    print("."*80)
+    return inputs_dictionary
+
+
+
+
+
+
+
