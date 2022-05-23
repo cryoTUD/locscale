e804b14c9be58ccfbeb4031e23b3f637f1709ff7
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-23 19:54:49 +0200

    import TFDS

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='locscale',
     description= 'Contrast optimization for cryo-EM maps',
     license='3-clause BSD',
     packages=setuptools.find_packages(),
-    install_requires=['matplotlib>=3.3.4','biopython>=1.78','numpy>=1.19.5','scipy>=1.5.4','pandas>=1.1.5','mrcfile>=1.3.0','gemmi>=0.4.8','pypdb>=2.0','sklearn>=0.0','pwlf>=2.0.4','tqdm>=4.62.3','more_itertools>=8.10.0','servalcat>=0.2.23','tensorflow==2.6.0','tensorflow-addons==0.14.0','keras==2.6.0','pyfiglet>=0.8.post1'],
+    install_requires=['matplotlib>=3.3.4','biopython>=1.78','numpy>=1.19.5','scipy>=1.5.4','pandas>=1.1.5','mrcfile>=1.3.0','gemmi>=0.4.8','pypdb>=2.0','sklearn>=0.0','pwlf>=2.0.4','tqdm>=4.62.3','more_itertools>=8.10.0','servalcat>=0.2.23','tensorflow==2.6.0','tensorflow-addons==0.14.0','keras==2.6.0','tensorflow_datasets==4.5.2','pyfiglet>=0.8.post1'],
     entry_points={
       'console_scripts': [
           'locscale = locscale.main:main',
