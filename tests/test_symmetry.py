cfb8460510bfb42cdf6082d16cc629000541fca7
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-24 15:54:39 +0200

    test_symmetry inside a temp directory

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -18,7 +18,16 @@ class TestSymmetry(unittest.TestCase):
         self.locscale_path = get_locscale_path()
         
         self.emmap_path = os.path.join(self.locscale_path,"tests","test_data","emd5778_map_full.mrc")
-                
+
+    def copy_files(self, file_path, tempDir):
+        from subprocess import run
+        run(["cp",file_path,tempDir])
+        if os.path.exists(os.path.join(tempDir, os.path.basename(file_path))):               
+            return os.path.join(tempDir, os.path.basename(file_path))
+            
+        else:
+            raise UserWarning("Could not copy {} to {}".format(path,tempDir))
+
         
     def test_symmetry(self):
         print("Imposing a symmetry condition of C4")
@@ -26,13 +35,11 @@ class TestSymmetry(unittest.TestCase):
         from tempfile import TemporaryDirectory
         
         with TemporaryDirectory() as tempDir:
-            import os
-            os.chdir(tempDir)
-            sym = em.symmetry_average([self.emmap_path],[3.4],pglist=["C4"])
+            copied_emmap_path = self.copy_files(self.emmap_path, tempDir)
+            sym = em.symmetry_average([copied_emmap_path],[3.4],pglist=["C4"])
             self.assertEqual(sym[0].shape,(256,256,256))
         
-
-        
+       
 
 if __name__ == '__main__':
     unittest.main()           
