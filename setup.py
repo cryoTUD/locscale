1926af348dc9484a1d08b568912641ce02a9818f
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-27 14:54:23 +0200

    setup.py changed to download models

--- setup.py
+++ setup.py
@@ -1,7 +1,33 @@
 from __future__ import division, absolute_import, print_function
 import setuptools
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
 from numpy.distutils.core import setup, Extension
 
+class PostDevelopCommand(develop):
+  """ Post-installation for installation mode. """
+  def run(self):
+    import os
+    develop.run(self)   
+    import locscale
+    locscale_path = os.path.dirname(locscale.__path__[0])
+    from locscale.utils.file_tools import download_emmernet_model_from_url, download_test_data_from_url, extract_tar_files_in_folder
+    ## Create folder to download emmernet models
+    emmernet_models_path = os.path.join(locscale_path, "locscale","emmernet", "emmernet_models")
+    os.makedirs(emmernet_models_path, exist_ok=True)
+    print("Downloading emmernet models")
+    download_emmernet_model_from_url(emmernet_models_path)
+    extract_tar_files_in_folder(emmernet_models_path)
+    ## Create folder to download test data 
+    test_data_path = os.path.join(locscale_path, "tests", "test_data")
+    os.makedirs(test_data_path, exist_ok=True)
+    print("Downloading test data")
+    download_test_data_from_url(test_data_path)
+    ## Extract tar.gz files in test data folder
+    extract_tar_files_in_folder(test_data_path)
+
+    
 
 setup(name='locscale',
     version='2.0',
@@ -16,5 +42,6 @@ setup(name='locscale',
           'locscale = locscale.main:main',
                           ],
       },
+      cmdclass={'develop': PostDevelopCommand},
     zip_safe= False)
 
