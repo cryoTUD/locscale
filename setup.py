76f6e525d9a6598780910150a17e685d18ce57a3
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-30 10:53:00 +0200

    Extraction of files happens in same folder

--- setup.py
+++ setup.py
@@ -13,19 +13,20 @@ class PostDevelopCommand(develop):
     import locscale
     locscale_path = os.path.dirname(locscale.__path__[0])
     from locscale.utils.file_tools import download_emmernet_model_from_url, download_test_data_from_url, extract_tar_files_in_folder
+  
     ## Create folder to download emmernet models
     emmernet_models_path = os.path.join(locscale_path, "locscale","emmernet", "emmernet_models")
     os.makedirs(emmernet_models_path, exist_ok=True)
     print("Downloading emmernet models")
     download_emmernet_model_from_url(emmernet_models_path)
-    extract_tar_files_in_folder(emmernet_models_path)
+    extract_tar_files_in_folder(emmernet_models_path, use_same_folder=True)
     ## Create folder to download test data 
     test_data_path = os.path.join(locscale_path, "tests", "test_data")
     os.makedirs(test_data_path, exist_ok=True)
     print("Downloading test data")
     download_test_data_from_url(test_data_path)
     ## Extract tar.gz files in test data folder
-    extract_tar_files_in_folder(test_data_path)
+    extract_tar_files_in_folder(test_data_path, use_same_folder=True)
 
     
 
