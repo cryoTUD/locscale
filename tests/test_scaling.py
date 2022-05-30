cc906c2c5881c70e7a4e77538cdba2474c01eeb2
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-30 17:59:26 +0200

    bug fixed in test_scaling

--- tests/test_scaling.py
+++ tests/test_scaling.py
@@ -40,7 +40,7 @@ class test_compute_scaling(unittest.TestCase):
         
     
     def test_scaling(self):
-        from locscale.utils.scaling_tools import compute_radial_profile, compute_scale_factors, set_radial_profile
+        from locscale.utils.scaling_tools import compute_radial_profile_proper, compute_scale_factors, set_radial_profile
         
         for i in [1,2,3]:
             print(i)
@@ -49,8 +49,8 @@ class test_compute_scaling(unittest.TestCase):
             modmap_window = self.test_data[i]['modmap_window']
             
             print(emmap_window.shape)
-            rp_emmap_test,_ = compute_radial_profile(emmap_window, self.frequency_map_window)
-            rp_modmap_test, frequencies_map = compute_radial_profile(modmap_window, self.frequency_map_window)
+            rp_emmap_test,_ = compute_radial_profile_proper(emmap_window, self.frequency_map_window)
+            rp_modmap_test, frequencies_map = compute_radial_profile_proper(modmap_window, self.frequency_map_window)
             rp_emmap_target = self.test_data[i]['rp_emmap']
             rp_modmap_target = self.test_data[i]['rp_modmap']
             
@@ -71,7 +71,7 @@ class test_compute_scaling(unittest.TestCase):
 
     
     def test_scaling_random(self):
-        from locscale.utils.scaling_tools import compute_radial_profile, compute_scale_factors, set_radial_profile
+        from locscale.utils.scaling_tools import compute_radial_profile_proper, compute_scale_factors, set_radial_profile
         from locscale.include.emmer.ndimage.map_tools import compute_real_space_correlation as rsc
         from locscale.include.emmer.ndimage.map_utils import extract_window
         import mrcfile
@@ -91,9 +91,9 @@ class test_compute_scaling(unittest.TestCase):
             emmap_window = extract_window(emmap, random_center_1, 40)
             modmap_window = extract_window(modmap, random_center_2, 40)
             
-            rp_emmap_window, _ = compute_radial_profile(emmap_window, self.frequency_map_window)
+            rp_emmap_window, _ = compute_radial_profile_proper(emmap_window, self.frequency_map_window)
             
-            rp_modmap_window, frequencies_map = compute_radial_profile(modmap_window, self.frequency_map_window)
+            rp_modmap_window, frequencies_map = compute_radial_profile_proper(modmap_window, self.frequency_map_window)
             
             sf_old,bfactor,qfit = compute_scale_factors(rp_emmap_window, rp_modmap_window, apix=self.apix, 
                                            scale_factor_arguments=self.scale_factor_arguments,
@@ -108,8 +108,8 @@ class test_compute_scaling(unittest.TestCase):
             scaled_map_old_inside,_ = set_radial_profile(emmap_window, sf_old, frequencies_map, self.frequency_map_window, emmap_window.shape)
             scaled_map_new_inside,_ = set_radial_profile(emmap_window, sf_new, frequencies_map, self.frequency_map_window, emmap_window.shape)
             
-            rp_scaled_map_old,_ = compute_radial_profile(scaled_map_old_inside, self.frequency_map_window)
-            rp_scaled_map_new,_ = compute_radial_profile(scaled_map_new_inside, self.frequency_map_window)
+            rp_scaled_map_old,_ = compute_radial_profile_proper(scaled_map_old_inside, self.frequency_map_window)
+            rp_scaled_map_new,_ = compute_radial_profile_proper(scaled_map_new_inside, self.frequency_map_window)
             
             radial_profile_match_old = rsc(rp_modmap_window,rp_scaled_map_old )
             radial_profile_match_new = rsc(rp_modmap_window,rp_scaled_map_new )
