554355cccd890119bbbe76432298eaa01b9d5054
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-05-31 18:20:00 +0200

    compiling fcodes within locscale

--- setup.py
+++ setup.py
@@ -5,37 +5,66 @@ from setuptools.command.develop import develop
 
 from numpy.distutils.core import setup, Extension
 
+def compile_fcodes_fast():
+  from numpy import f2py
+  from locscale.utils.file_tools import get_locscale_path
+  import os
+
+  fcodes_path = os.path.join(get_locscale_path(), 'include', 'fcodes_fast.f90')
+
+  with open(fcodes_path) as sourcefile:
+    sourcecode = sourcefile.read()
+  
+  f2py.compile(sourcecode, modulename='fcodes_fast')
+
+def download_emmernet_models():
+  from locscale.utils.file_tools import get_locscale_path, download_emmernet_model_from_url, extract_tar_files_in_folder
+  import os
+
+  ## Create folder to download emmernet models
+  emmernet_models_path = os.path.join(get_locscale_path(), "locscale","emmernet", "emmernet_models")
+  if not os.path.exists(emmernet_models_path):
+    os.makedirs(emmernet_models_path, exist_ok=True)
+    download_emmernet_model_from_url(emmernet_models_path)
+    extract_tar_files_in_folder(emmernet_models_path, use_same_folder=True)
+
+def download_test_data():
+  from locscale.utils.file_tools import get_locscale_path, download_test_data_from_url, extract_tar_files_in_folder
+  import os
+
+  ## Create folder to download tests_data
+  test_data_path = os.path.join(get_locscale_path(), "tests","test_data")
+  if not os.path.exists(test_data_path):
+    os.makedirs(test_data_path, exist_ok=True)
+    download_test_data_from_url(test_data_path)
+    extract_tar_files_in_folder(test_data_path, use_same_folder=True)
+
+def update_conda_environment():
+  import subprocess
+  from locscale.utils.file_tools import get_locscale_path
+
+  subprocess.run(["conda", "env", "update", "--file", os.path.join(get_locscale_path(), "environment.yml")])
+
+    
 class PostDevelopCommand(develop):
   """ Post-installation for development mode. """
   def run(self):
     import os
     develop.run(self)   
-    import locscale
-    locscale_path = os.path.dirname(locscale.__path__[0])
-    from locscale.utils.file_tools import download_emmernet_model_from_url, download_test_data_from_url, extract_tar_files_in_folder
-  
-    ## Create folder to download emmernet models
-    emmernet_models_path = os.path.join(locscale_path, "locscale","emmernet", "emmernet_models")
-    os.makedirs(emmernet_models_path, exist_ok=True)
-    print("Downloading emmernet models")
-    download_emmernet_model_from_url(emmernet_models_path)
-    extract_tar_files_in_folder(emmernet_models_path, use_same_folder=True)
-    ## Create folder to download test data 
-    test_data_path = os.path.join(locscale_path, "tests", "test_data")
-    os.makedirs(test_data_path, exist_ok=True)
-    print("Downloading test data")
-    download_test_data_from_url(test_data_path)
-    ## Extract tar.gz files in test data folder
-    extract_tar_files_in_folder(test_data_path, use_same_folder=True)
 
-    ## Update the current conda environment with the new packages in environment.yml
-    import subprocess
-    subprocess.run(["conda","env","update","--file",os.path.join(locscale_path,"environment.yml")])
+    # Download emmernet models
+    download_emmernet_models()
 
-    ## Install EMDA and Proshade
-    subprocess.run(["pip","install","emda==1.1.3.post6"])
-    subprocess.run(["pip","install","proshade==0.7.3"])
+    # Download test data
+    download_test_data()
 
+    # Update conda environment
+    update_conda_environment()
+
+    # Compile fcodes_fast
+    compile_fcodes_fast()
+
+    
     
 
 setup(name='locscale',
@@ -45,7 +74,10 @@ setup(name='locscale',
     description= 'Contrast optimization for cryo-EM maps',
     license='3-clause BSD',
     packages=setuptools.find_packages(),
-    install_requires=['matplotlib>=3.3.4','biopython>=1.78','numpy>=1.19.5','scipy>=1.5.4','pandas>=1.1.5','mrcfile>=1.3.0','gemmi>=0.4.8','pypdb>=2.0','sklearn>=0.0','pwlf>=2.0.4','tqdm>=4.62.3','more_itertools>=8.10.0','servalcat>=0.2.23','tensorflow==2.6.0','tensorflow-addons==0.14.0','keras==2.6.0','tensorflow_datasets==4.5.2','pyfiglet>=0.8.post1'],
+    install_requires=['matplotlib>=3.3.4','biopython>=1.78','numpy>=1.19.5','scipy>=1.5.4','pandas>=1.1.5',\
+                      'mrcfile>=1.3.0','gemmi>=0.4.8','pypdb>=2.0','sklearn>=0.0','pwlf>=2.0.4','tqdm>=4.62.3',\
+                      'more_itertools>=8.10.0','servalcat>=0.2.23','tensorflow==2.6.0','tensorflow-addons==0.14.0',\
+                        'keras==2.6.0','tensorflow_datasets==4.5.2','pyfiglet>=0.8.post1'],
     entry_points={
       'console_scripts': [
           'locscale = locscale.main:main',
