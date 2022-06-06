fe4e9a85cd31c51f717fdc518c2d312abd8e6e40
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-06-07 00:27:28 +0200

    working trilinear interpolator using for loops

--- locscale/include/symmetry_emda/apply_rotation_matrix.py
+++ locscale/include/symmetry_emda/apply_rotation_matrix.py
@@ -12,24 +12,26 @@ def get_spherical_mask(shape, radius_pixels):
 
 def transform_origin_to_center_of_image(coordinates, center, shape):
     """
-    Transform a set of 3D numpy indices to XYZ coordinates, with the origin at the center of the image
+    Transform a set of 3D numpy indices to hkl coordinates, with the origin at the center of the image
     """
     import numpy as np
     coordinates = np.array(coordinates)
     center = np.array(center)
     transformed_coordinates = coordinates - center  ## still in ZYX
-    # convert to XYZ
-    xyz_transformed_coordinates = np.flip(transformed_coordinates,axis=1)
+    # convert to hkl
+    hkl_transformed_coordinates = np.flip(transformed_coordinates, axis=-1) ## now XYZ > HKL
+    hkl_transformed_coordinates[:,1] *= -1
+    #print((hkl_transformed_coordinates[:,2]==transformed_coordinates[:,0]).all())
+    #print((hkl_transformed_coordinates[:,0]==transformed_coordinates[:,2]).all())
     # Invert y axis
-    invert_matrix = np.ones(transformed_coordinates.shape)
-    invert_matrix[:,1] = -1
-    invert_y_axis = xyz_transformed_coordinates * invert_matrix
     
-    return np.clip(invert_y_axis, 0, shape[0]//2-1)
+    return hkl_transformed_coordinates
+    #return np.clip(invert_y_axis, 0, shape[0]//2-1)
+
 
 def transform_coordinates_to_numpy_coordinates(coordinates, center, shape):
     """
-    Transform a set of 3D numpy indices to XYZ coordinates, with the origin at the center of the image
+    Transform a set of 3D numpy indices to hkl coordinates, with the origin at the center of the image
     """
     import numpy as np
     coordinates = np.array(coordinates)
@@ -41,30 +43,187 @@ def transform_coordinates_to_numpy_coordinates(coordinates, center, shape):
     
 def transform_coordinates_using_rotation_matrix(coordinates, rotation_matrix):
     """
-    Transform a set of XYZ coordinates using a rotation matrix
+    Transform a set of hkl coordinates using a rotation matrix
     """
     import numpy as np
     coordinates = np.array(coordinates)
-    coordinates = np.dot(coordinates, rotation_matrix)
-    return coordinates 
-
-def trilinear_interpolation(FT, rotation_matrix):
-    """
-    Perform trilinear interpolation on a 3D FT image
-    """
-    import numpy as np
-    import numpy.linalg as la
-    from scipy.interpolate import interpn
+    coordinates_T = np.matmul(rotation_matrix, np.transpose(coordinates))
+    coordinates_T = np.transpose(coordinates_T)
+        
     
-    rotation_matrix = np.transpose(rotation_matrix)
+    return coordinates_T 
+
+def get_depth_row_col_ix_point(x,center):
+    nz, ny, nx = center
+    
+    col_ix = int(x[0]+nx//2)
+    row_ix = int(ny//2 - x[1])
+    depth_ix = int(x[2]+nz//2)
+    
+    return depth_ix, row_ix, col_ix
+
+def get_depth_row_col_ix(x,center):
+    nz, ny, nx = center
+    
+    col_ix = x[:,0]+nx//2
+    row_ix = ny//2 - x[:,1]
+    depth_ix = x[:,2]+nz//2
+    
+    return depth_ix.astype(int), row_ix.astype(int), col_ix.astype(int)
+    
+def trilinear_interpolation(FT, rotation_matrix):
+    import numpy as np
+    from tqdm import tqdm
+    from scipy.interpolate import RegularGridInterpolator
+    #from scipy.iinterpolate import interpn
+    
+    
+    nz, ny, nx = FT.shape
+    radius_pixels = nx//2
+    spherical_mask = get_spherical_mask(FT.shape, radius_pixels).astype(bool)
+    interpolate_FT = np.zeros(FT.shape, dtype="complex_")
+    
+    zlims = np.linspace(int(-nz//2), int(nz//2)-1, nz)
+    ylims = np.linspace(int(-ny//2), int(ny//2)-1, ny)
+    xlims = np.linspace(int(-nx//2), int(nx//2)-1, nx)
+    
+    interpolator = RegularGridInterpolator(points=(zlims, ylims, xlims), \
+                                                  values=FT, \
+                                                  method='linear')
+    
+    rotation_matrix_transpose = np.transpose(rotation_matrix)
+    center = (nz//2, ny//2, nx//2)
+    pbar = tqdm(total=nx//2*ny*nz, desc="Symmetrising: ")
+    slist = []
+    cdlist = []
+    conjlist = []
+    compare_interpolation_methods = []
+    for depth in range(nz):
+        for row in range(ny):
+            for col in range(nx//2):
+                pbar.update(1)
+                if spherical_mask[depth,row,col]: 
+                        
+                    h = col-nx//2 ## center
+                    k = -1 * (row-ny//2) ## flip Y for correct axis
+                    l = depth - nz//2 
+                    slist.append(np.array([h,k,l]))
+                    cdlist.append(np.array([depth, row, col]))
+                    
+                    s = np.array([h,k,l])
+                    x = np.matmul(rotation_matrix_transpose, s)
+                    x0 = np.floor(x)
+                    x1 = x0+1
+                    xd = x-x0
+                    xdr = 1 - xd
+                    assert (xd<1).all(), "xd: {}".format(xd)
+                    
+                    d0, r0, c0 = get_depth_row_col_ix_point(x0, center)
+                    d1, r1, c1 = get_depth_row_col_ix_point(x1, center)
+                    
+                                        
+                    c000 = FT[d0,r0,c0]
+                    c001 = FT[d0,r0,c1]
+                    c010 = FT[d0,r1,c0]
+                    c011 = FT[d0,r1,c1]
+                    c100 = FT[d1,r0,c0]
+                    c101 = FT[d1,r0,c1]
+                    c110 = FT[d1,r1,c0]
+                    c111 = FT[d1,r1,c1]
+                    
+                    c00 = c000*xdr[0] + c100*xd[0]
+                    c01 = c001*xdr[0] + c101*xd[0]
+                    c10 = c010*xdr[0] + c110*xd[0]
+                    c11 = c011*xdr[0] + c111*xd[0]
+                    
+                    c0 = c00*xdr[1] + c10*xd[1]
+                    c1 = c01*xdr[1] + c11*xd[1]
+                    
+                    c = c0*xdr[2] + c1*xd[2]
+                    
+                    c_scipy = interpolator(np.flip(x))
+                    #compare_interpolation_methods.append(c==c_scipy)
+                    if c != c_scipy:
+                        print("not equal")
+                        print(c)
+                        print(c_scipy)
+                        print(h,k,l)
+                    if c == c_scipy:
+                        print("equal")
+                        print("here > ",h,k,l)
+                    
+                    dC, rC, cC = get_depth_row_col_ix_point(-1*s, center)
+                    conjlist.append(np.array([dC,rC,cC]))
+                    interpolate_FT[depth, row, col] = c
+                    interpolate_FT[dC, rC, cC] = np.conjugate(c)
+                    
+                                        
+                else:
+                    continue
+    
+    slist = np.array(slist)
+    print("smin" , slist.min())
+    print("smax" , slist.max())
+    cdlist = np.array(cdlist)
+    conjlist = np.array(conjlist)
+    compare_interpolation_methods = np.array(compare_interpolation_methods)
+    
+    test_hkl_transform = transform_origin_to_center_of_image(cdlist, center, FT.shape)
+    
+    dtest, rtest, ctest = get_depth_row_col_ix(-1*slist, center)
+    
+    
+    print("coordinate check: ",(test_hkl_transform==slist).all())
+    
+    print("conjugate coordinate check D: ", (dtest==conjlist[:,0]).all())
+    print("conjugate coordinate check R: ", (rtest==conjlist[:,1]).all())
+    print("conjugate coordinate check C: ", (ctest==conjlist[:,2]).all())
+    
+    print("interpolation alg check: ", compare_interpolation_methods.all())
+    print("is nan? ",np.isnan(interpolate_FT).any())
+        
+    x_slice_FT = abs(FT[:,:,140])
+    x_slice_interpolated_FT = abs(interpolate_FT[:,:,140])
+    
+    y_slice_FT = abs(FT[:,140,:])
+    y_slice_interpolated_FT = abs(interpolate_FT[:,140,:])
+    
+    z_slice_FT = abs(FT[140,:,:])
+    z_slice_interpolated_FT = abs(interpolate_FT[140,:,:])
+    
+    from locscale.include.emmer.ndimage.map_tools import compute_real_space_correlation as rsc
+    
+    print("roptation: \n", rotation_matrix.round(2))
+    
+    rscc_slice_x = rsc(x_slice_FT, x_slice_interpolated_FT)
+    rscc_slice_y = rsc(y_slice_FT, y_slice_interpolated_FT)
+    rscc_slice_z = rsc(z_slice_FT, z_slice_interpolated_FT)
+    
+    print("rscc x slice:",rscc_slice_x)
+    print("rscc y slice:",rscc_slice_y)
+    print("rscc z slice:",rscc_slice_z)
+    
+    return interpolate_FT
+                
+
+def trilinear_interpolation2(FT, rotation_matrix):
+
+    #Perform trilinear interpolation on a 3D FT image
+
+    import numpy as np
+    #import numpy.linalg as la
+    from scipy.interpolate import interpn
+    from scipy.interpolate import RegularGridInterpolator
+    
+    #rotation_matrix = np.transpose(rotation_matrix)
     ## FT = Fourrier transform of the map
 
     # Get the shape of the FT image
     nz, ny, nx = FT.shape
 
-    depth, row, column = np.mgrid[:nz, :ny, :nx]
-    numpy_coordinates = np.column_stack((depth.ravel(), row.ravel(), column.ravel()))  ## numpy_coordinates is ZYX format
-    values_at_coordinates = FT.ravel()
+    #depth, row, column = np.mgrid[:nz, :ny, :nx]
+    #numpy_coordinates = np.column_stack((depth.ravel(), row.ravel(), column.ravel()))  ## numpy_coordinates is ZYX format
+    #values_at_coordinates = FT.ravel()
 
     # Limit calculation to one half of a spherical mask within the FT
 
@@ -79,30 +238,36 @@ def trilinear_interpolation(FT, rotation_matrix):
     hemisphere_mask[:,:,x_min:x_max] = False
 
     # Coordinates of hemisphere mask where True
-    hemisphere_coordinates = np.asarray(np.where(hemisphere_mask)).T
+    hemisphere_coordinates = np.asarray(np.where(hemisphere_mask)).T  # ZYX
     
 
-    # Convert hemisphere coordinates to XYZ coordinates
+    # Convert hemisphere coordinates to hkl coordinates
     center = np.array([nx//2, ny//2, nz//2])
-    xyz_hemisphere_coordinates = transform_origin_to_center_of_image(hemisphere_coordinates, center, FT.shape)
+    hkl_hemisphere_coordinates = transform_origin_to_center_of_image(hemisphere_coordinates, center, FT.shape)
     print("Transformed hemisphere coordinates")
     
-    h, k, l = xyz_hemisphere_coordinates.T.astype(int)
-    hT, kT, lT = -1*h, -1*k, -1*l
-    xyz_hemisphere_coordinates_conjugate = -1 * xyz_hemisphere_coordinates
-    conjugate_coordinates = transform_coordinates_to_numpy_coordinates(xyz_hemisphere_coordinates_conjugate, center, FT.shape).astype(int)
-    z_conj, y_conj, x_conj = conjugate_coordinates.T
-    conjugate_mask = np.zeros(FT.shape)
-    conjugate_mask[z_conj, y_conj, x_conj] = 1
-    conjugate_mask = conjugate_mask.astype(bool)
+    
+    
+#    hkl_hemisphere_coordinates_conjugate = -1 * hkl_hemisphere_coordinates
+#    conjugate_coordinates = transform_coordinates_to_numpy_coordinates(hkl_hemisphere_coordinates_conjugate, center, FT.shape).astype(int)
+#    z_conj, y_conj, x_conj = conjugate_coordinates.T
+#    conjugate_mask = np.zeros(FT.shape)
+#    conjugate_mask[z_conj, y_conj, x_conj] = 1
+#    conjugate_mask = conjugate_mask.astype(bool)
     
 
-    # Transform XYZ hemisphere coordinates using rotation matrix
-    xyz_hemisphere_coordinates_transform = transform_coordinates_using_rotation_matrix(xyz_hemisphere_coordinates, rotation_matrix)
-    print("Transformed XYZ hemisphere coordinates")
+    # Transform hkl hemisphere coordinates using rotation matrix
+    hkl_hemisphere_coordinates_transform = transform_coordinates_using_rotation_matrix(hkl_hemisphere_coordinates, rotation_matrix)
+    hkl_hemisphere_coordinates_transform = np.clip(hkl_hemisphere_coordinates_transform, int(-nz//2), int(nz//2)-1)
+    print("Transformed hkl hemisphere coordinates")
+    #print(hkl_hemisphere_coordinates_transform[:,0].min(), hkl_hemisphere_coordinates_transform[:,0].max())
+    #print(hkl_hemisphere_coordinates_transform[:,1].min(), hkl_hemisphere_coordinates_transform[:,1].max())
+    #print(hkl_hemisphere_coordinates_transform[:,2].min(), hkl_hemisphere_coordinates_transform[:,2].max())
+    
+    
 
     # Interpolation points in numpy coordinates
-    #interpolation_points = transform_coordinates_to_numpy_coordinates(xyz_hemisphere_coordinates_transform, center, FT.shape)
+    #interpolation_points = transform_coordinates_to_numpy_coordinates(hkl_hemisphere_coordinates_transform, center, FT.shape)
    # print("Transformed interpolation points")#
 
     # Interpolate
@@ -110,19 +275,43 @@ def trilinear_interpolation(FT, rotation_matrix):
     ylims = np.linspace(int(-ny//2), int(ny//2)-1, ny)
     xlims = np.linspace(int(-nx//2), int(nx//2)-1, nx)
     
-    interpolated_values = interpn((zlims, ylims, xlims), FT, xyz_hemisphere_coordinates_transform, method='linear')
+    interpolator = RegularGridInterpolator(points=(zlims, ylims, xlims), \
+                                                  values=FT, \
+                                                  method='linear')
+    interpolated_values = interpolator(hkl_hemisphere_coordinates_transform)
     print("Interpolated values")
 
     # Apply interpolated values to a new FT image
     interpolated_FT = np.zeros(FT.shape,dtype="complex_")
     interpolated_FT[hemisphere_mask] = interpolated_values
-
-    interpolated_FT[lT,kT,hT] = np.conjugate(interpolated_FT[l,k,h])
-    print("is nan? ",np.isnan(interpolated_FT).any())
-
-    return interpolated_FT
+    depth, row, col = get_depth_row_col_ix(hkl_hemisphere_coordinates, center)
+    depth_T, row_T, col_T = get_depth_row_col_ix(-1*hkl_hemisphere_coordinates, center)
     
-
+    interpolated_FT[depth_T,row_T,col_T] = np.conjugate(interpolated_FT[depth,row,col])
+    print("is nan? ",np.isnan(interpolated_FT).any())
+        
+    x_slice_FT = abs(FT[:,:,140])
+    x_slice_interpolated_FT = abs(interpolated_FT[:,:,140])
+    
+    y_slice_FT = abs(FT[:,140,:])
+    y_slice_interpolated_FT = abs(interpolated_FT[:,140,:])
+    
+    z_slice_FT = abs(FT[140,:,:])
+    z_slice_interpolated_FT = abs(interpolated_FT[140,:,:])
+    
+    from locscale.include.emmer.ndimage.map_tools import compute_real_space_correlation as rsc
+    
+    print("roptation: \n", rotation_matrix.round(2))
+    
+    rscc_slice_x = rsc(x_slice_FT, x_slice_interpolated_FT)
+    rscc_slice_y = rsc(y_slice_FT, y_slice_interpolated_FT)
+    rscc_slice_z = rsc(z_slice_FT, z_slice_interpolated_FT)
+    
+    print("rscc x slice:",rscc_slice_x)
+    print("rscc y slice:",rscc_slice_y)
+    print("rscc z slice:",rscc_slice_z)
+    return interpolated_FT
+   
 
 
     
@@ -175,10 +364,10 @@ def trilinear_interpolation(FT, rotation_matrix):
 #     numpy_coordinates = np.asarray(np.where(spherical_indices)).T
 #     values_in_spherical_mask = FT[spherical_indices] ## numpy coordinates are in the form of (z,y,x)
 
-#     ## Convert numpy coordinates to XYZ coordinates
+#     ## Convert numpy coordinates to hkl coordinates
 #     # Get the center of the image
 #     center = np.array([nz//2, ny//2, nx//2])
-#     # Transform the numpy coordinates to XYZ coordinates
+#     # Transform the numpy coordinates to hkl coordinates
 #     LKH_coordinates_full = transform_coordinates_to_center_of_image(numpy_coordinates, center)
    
 #     # Make values of mask corresponding to negative X frequencies zero
@@ -190,7 +379,7 @@ def trilinear_interpolation(FT, rotation_matrix):
 #     # Obtain coordinates of all non-zero pixels in the spherical mask
 #     numpy_coordinates_halfmask = np.asarray(np.where(spherical_indices_halfmask)).T
            
-#     # Transform the coordinates of the spherical mask to XYZ coordinates
+#     # Transform the coordinates of the spherical mask to hkl coordinates
 #     LKH_coordinates_halfmask = transform_coordinates_to_center_of_image(numpy_coordinates_halfmask, center)
 
 #     # Find the symmetrically corresponding coordinates in the FT image using the rotation matrix
