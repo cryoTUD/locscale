ed4400ca78fcaf4911e2026d9335e2b259ce31ed
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-06-09 17:59:07 +0200

    Numpy 1.20

--- setup.py
+++ setup.py
@@ -132,9 +132,6 @@ class PostInstallCommand(install):
         # Run tests
         run_locscale_tests()
 
-        
-    
-
 setup(name='locscale',
     version='2.0',
     author='Alok Bharadwaj, Arjen J. Jakobi, Reinier de Bruin and Carsten Sachse',
@@ -142,7 +139,7 @@ setup(name='locscale',
     description= 'Contrast optimization for cryo-EM maps',
     license='3-clause BSD',
     packages=setuptools.find_packages(),
-    install_requires=['matplotlib>=3.3.4','biopython>=1.78','numpy>=1.19.5','scipy>=1.5.4','pandas>=1.1.5',\
+    install_requires=['matplotlib>=3.3.4','biopython>=1.78','numpy>=1.20','scipy>=1.5.4','pandas>=1.1.5',\
                       'mrcfile>=1.3.0','gemmi>=0.4.8','pypdb>=2.0','sklearn>=0.0','pwlf>=2.0.4','tqdm>=4.62.3',\
                       'more_itertools>=8.10.0','servalcat>=0.2.23','tensorflow==2.6.0','tensorflow-addons==0.14.0',\
                         'keras==2.6.0','tensorflow_datasets==4.5.2','pyfiglet>=0.8.post1'],
