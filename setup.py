d854e3ee98e7065aae142645c314f5050a4330cc
abharadwaj1 <a.bharadwaj@tudelft.nl>
2022-11-08 17:36:23 +0100

    version 2.1.5

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ class PostInstallCommand(install):
 
 
 setup(name='locscale',
-    version='2.1.4',
+    version='2.1.5',
     author='Alok Bharadwaj, Arjen J. Jakobi, Reinier de Bruin',
     url='https://gitlab.tudelft.nl/aj-lab/locscale',
     project_urls={
