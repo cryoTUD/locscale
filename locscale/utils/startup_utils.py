c054dc39429b5a513b095e6fcbe8b2fee7625656
abharadwaj1 <abharadwaj1@tudelft.nl>
2024-10-16 17:40:22 +0200

    fixed pVDDT.mrc in print statement

--- locscale/utils/startup_utils.py
+++ locscale/utils/startup_utils.py
@@ -242,7 +242,7 @@ def launch_feature_enhance(args):
     try_to(compute_probability_distribution,**probability_args)
     
     print("EMmerNet finished successfully")
-    print("Please check the pVDDT_scores.mrc to observe significantly hallucinated features")
+    print("Please check the pVDDT.mrc to observe significantly hallucinated features")
     ## Print end
     print_end_banner(datetime.now(), start_time)
     
