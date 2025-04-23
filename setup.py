184efd0e8ba545bebfc7086d008dcf090a5d3045
abharadwaj1 <abharadwaj1@tudelft.nl>
2025-04-23 08:01:06 +0200

    mac support using tf 2 .13

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ else:
     ext_modules = [ex1]
 
 setup(name='locscale',
-    version="2.3",
+    version="2.3.0.post1",
     author='Alok Bharadwaj, Arjen J. Jakobi, Reinier de Bruin',
     url='https://gitlab.tudelft.nl/aj-lab/locscale',
     project_urls={
