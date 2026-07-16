239ca4892c9a969bebb6b744fc8fe0ad484b57fc
abharadwaj1 <abharadwaj1@tudelft.nl>
2026-07-16 23:01:17 +0200

    fix torch paths

--- locscale/emmernet/utils.py
+++ locscale/emmernet/utils.py
@@ -150,8 +150,8 @@ def check_and_download_emmernet_model(verbose=False):
 
     emmernet_model_folder = os.path.join(os.path.dirname(locscale.__file__), "emmernet", "emmernet_models")
     path_exists = os.path.exists(emmernet_model_folder)
-    EMMERNET_HIGH_CONTEXT_MODEL_DOWNLOADED = os.path.exists(os.path.join(emmernet_model_folder, "emmernet", "EMmerNet_highContext.hdf5"))
-    EMMERNET_LOW_CONTEXT_MODEL_DOWNLOADED = os.path.exists(os.path.join(emmernet_model_folder, "emmernet", "EMmerNet_lowContext.hdf5"))
+    EMMERNET_HIGH_CONTEXT_MODEL_DOWNLOADED = os.path.exists(os.path.join(emmernet_model_folder, "emmernet", "emmernet_highcontext.pt"))
+    EMMERNET_LOW_CONTEXT_MODEL_DOWNLOADED = os.path.exists(os.path.join(emmernet_model_folder, "emmernet", "emmernet_lowcontext.pt"))
     
 
     emmernet_downloaded = path_exists and EMMERNET_HIGH_CONTEXT_MODEL_DOWNLOADED and EMMERNET_LOW_CONTEXT_MODEL_DOWNLOADED
